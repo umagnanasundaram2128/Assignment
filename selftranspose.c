0a167b36b97bf02a8a806b043d32ccccae3cf90e
uma g <umagnanasundaram2128@users.noreply.github.com>
2015-12-26 19:40:39 +0530

    Update selftranspose.c

--- selftranspose.c
+++ selftranspose.c
@@ -2,37 +2,35 @@
 int a[10][10];
 void swap(int i,int j)
 {
-	int t;
-	t=a[i][j];
-	a[i][j]=a[j][i];
-	a[j][i]=t;
+	a[i][j]+=a[j][i];
+	a[j][i]=a[i][j]-a[j][i];
+	a[i][j]-=a[j][i];
 }
 int main()
 {
-	//int a[10][10];
 	int i,j,m,n;
-	printf("Enter the order of matrix 'm' and 'n':");
-	scanf("%d%d",&m,&n);
+	printf("Enter the order of matrix 'n':");
+	scanf("%d",&n);
 	printf("Enter the matrix elements:");
-	for(i=0;i<m;i++)	
+	for(i=0;i<n;i++)	
 	{
 		for(j=0;j<n;j++)
 		{
 			scanf("%d",&a[i][j]);
 		}
 	}
-	for(i=0;i<m;i++)
+	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
 		{
-			if(i<j)
+			if(i<j) 
 			{
 				swap(i,j);
 			}
 		}
 	}
 
-	for(i=0;i<m;i++)
+	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
 		{
