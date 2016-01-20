297243ab0acf797f62b6b81750f00a69048c7ecf
Uma G <umagnanasundaram2128@users.noreply.github.com>
2016-01-21 00:03:33 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void rev(int num)
+{
+	int remainder,rev_num;
+	while(num>0)
+	{
+		remainder=num%10;
+		num=num/10;
+		rev_num=remainder+(rev_num*10);
+	}
+	printf("\n%d",rev_num);
+}
+
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	rev(num);
+	return 0;
+}
